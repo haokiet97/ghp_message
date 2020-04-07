d05fd2ae1fcd7a0968aa3ecacbe6e65c7f6e78d9
haokiet97 <33271409+haokiet97@users.noreply.github.com>
2020-04-07 18:13:21 +0700

    chat realtime 2 users

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Base(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Message(Base):
+    author = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.author.username
+
+    def last_10_messages():
+        return Message.objects.order_by('-created_at').all()[:10]
+
